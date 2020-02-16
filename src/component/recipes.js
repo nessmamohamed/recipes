4960565242a6c43729c4fa85a1ba682076181b98
nesso mohamed <nessomohamed555@gmail.com>
2020-02-16 19:15:53 +0200

    make it better

--- src/component/recipes.js
+++ src/component/recipes.js
@@ -12,7 +12,7 @@ const Recipes = () => {
      
 
     const [recipes , setReccipes] = useState([])
-    const [recipe , setReccipe] = useState(localStorage.recipe.length <= 2 ? '' : JSON.parse(localStorage.getItem('recipe')))
+    const [recipe , setReccipe] = useState(!localStorage.recipe.length >= 2 ? '' : JSON.parse(localStorage.getItem('recipe')))
     const [diet, setDiet] = useState( localStorage.diet.length <= 2 ? [] : JSON.parse(localStorage.getItem('diet')) )
     const [health , setHealth] = useState( localStorage.health.length <=2 ? ['alcohol-free'] : JSON.parse(localStorage.getItem('health')))
     const [loaded, setLoaded] = useState(false)
