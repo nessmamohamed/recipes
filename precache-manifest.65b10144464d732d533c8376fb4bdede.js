dd016c62b65e6fba5fffa4db9904b23aad3b269f
nesso mohamed <nessomohamed555@gmail.com>
2020-02-28 15:49:00 +0200

    Updates

---
+++ precache-manifest.65b10144464d732d533c8376fb4bdede.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a80a7495acdf726bce6b24bc606d79b6",
+    "url": "/recipes/index.html"
+  },
+  {
+    "revision": "d4186afa96f0b11e227e",
+    "url": "/recipes/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "cbbb5449e63c1f0b75a7",
+    "url": "/recipes/static/css/main.86d6b200.chunk.css"
+  },
+  {
+    "revision": "d4186afa96f0b11e227e",
+    "url": "/recipes/static/js/2.8bc5aa7d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/recipes/static/js/2.8bc5aa7d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cbbb5449e63c1f0b75a7",
+    "url": "/recipes/static/js/main.9413ac69.chunk.js"
+  },
+  {
+    "revision": "12688874dbb127aa4270",
+    "url": "/recipes/static/js/runtime-main.aab8fb32.js"
+  },
+  {
+    "revision": "403399ad33775074ffe886b70dff4094",
+    "url": "/recipes/static/media/recipe.403399ad.png"
+  }
+]);
